59888014ae2b1a9b9247ccde7f84bb2d5482eec2
DarkPhisyc <darkphisyc50@gmail.com>
2026-03-03 18:59:16 -0600

    Etiquetas, una prueba pequeña y algo más

--- src/Gestion.java
+++ src/Gestion.java
@@ -3,10 +3,28 @@ import net.objecthunter.exp4j.ExpressionBuilder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 public class Gestion implements ActionListener {
+    private JTextField funcion;
+
+    public Gestion(JTextField funcion) {
+        this.funcion = funcion;
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+        try {
+            String f = funcion.getText().trim();
+            Expression exp = new ExpressionBuilder(f)
+                .variable("x")
+                .build();
+                exp.setVariable("x", 2);
+            double resultado = exp.evaluate();
+            JOptionPane.showMessageDialog(null, "El resultado de evaluar la función es: " + resultado);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error al evaluar la función.", null, JOptionPane.ERROR_MESSAGE);
+        }
     }
     
 }
--- src/Ventana.java
+++ src/Ventana.java
@@ -1,25 +1,55 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class Ventana extends JFrame {
     private JTextField funcion;
     private JButton calcular;
+    private JLabel etiqueta1;
+    private JLabel etiqueta2;
+    private JLabel etiqueta3;
+    private JTextField a;
+    private JTextField b;
 
     public Ventana(int x, int y, int alto, int ancho, String titulo) {
         // Generación de la ventana
         super(titulo);
         setBounds(x, y, alto, ancho);
         setLayout(null);
-        setVisible(true);
+        
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         // Campo de texto para la función
         funcion = new JTextField();
-        funcion.setBounds(190, 100, 180, 30);
+        funcion.setBounds(190, 50, 180, 30);
         add(funcion);
         // Botón para calcular
         calcular = new JButton("Calcular");
-        calcular.setBounds(229, 140, 100, 30);
+        calcular.setBounds(229, 200, 100, 30);
         add(calcular);
+        // Agregar acción al botón
+        Gestion gestion = new Gestion(funcion);
+        calcular.addActionListener(gestion);
+        // Etiqueda para indicar la entrada de la función
+        etiqueta1 = new JLabel("Ingresa la función f(x):");
+        etiqueta1.setBounds(60, 50, 150, 30);
+        add(etiqueta1);
+        // Etiqueda para indicar la entrada del intervalo
+        etiqueta2 = new JLabel("Ingresa el intervalo [a, b]:");
+        etiqueta2.setBounds(41, 100, 150, 30);
+        add(etiqueta2);
+        // Campos de texto para el intervalo
+        a = new JTextField();
+        a.setBounds(190, 100, 80, 30);
+        add(a);
+        b = new JTextField();
+        b.setBounds(290, 100, 80, 30);
+        add(b);
+        // Una coma
+        etiqueta3 = new JLabel(",");
+        etiqueta3.setBounds(275, 100, 20, 30);
+        add(etiqueta3);
+
+        setVisible(true);
     }
 }
